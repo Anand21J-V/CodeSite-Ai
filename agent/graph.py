aaabe299dce54b4e7758287a1d3370274c33e79b
Anand21J-V <anandvishwakarma21j.com>
2026-05-02 23:20:57 +0530

    Planner agent and Architect Agent Complete

---
+++ agent/graph.py
@@ -0,0 +1,47 @@
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+from pydantic import BaseModel, Field
+from prompts import planner_agent_prompt, architect_agent_prompt
+from states import Planner_schema, TaskPlan
+from langgraph.graph import StateGraph, START, END
+
+load_dotenv()
+
+llm = ChatGroq(model="llama-3.3-70b-versatile")
+# model = openai/gpt-oss-120b
+
+def planner_agent(state: dict) -> dict:
+    user_prompt = state["user_prompt"]
+    response = llm.with_structured_output(Planner_schema).invoke(planner_agent_prompt(user_prompt))
+    if response is None:
+        raise ValueError("Planner did not return a Valid Response")
+    
+    return {"plan": response}
+
+def architect_agent(state: dict) -> dict:
+    plan = state["plan"]
+    response = llm.with_structured_output(TaskPlan).invoke(architect_agent_prompt(plan)) 
+    if response is None:
+        raise ValueError("Architect did not return a Valid Response")
+    
+    # For Context Engineering adding the previous State As well
+    response.plan = plan
+
+    return {"task_plan": response}
+
+
+graph = StateGraph(dict)
+graph.add_node("planner", planner_agent)
+graph.add_node("architect", architect_agent)
+graph.add_edge(start_key="planner", end_key="architect")
+
+graph.set_entry_point("planner")
+
+agent = graph.compile()
+
+
+if __name__ == "__main__":
+    user_prompt = "Create a simple calculator web Application"
+    result = agent.invoke({"user_prompt": user_prompt})
+
+    print(result)
